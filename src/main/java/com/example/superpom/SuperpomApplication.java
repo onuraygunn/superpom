4f3c8e33d7557fdc34733daa7ed161119c811111
Onur Aygün <onur.aygun@innovance.com.tr>
2024-01-08 13:53:51 +0300

    init

---
+++ src/main/java/com/example/superpom/SuperpomApplication.java
@@ -0,0 +1,13 @@
+package com.example.superpom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperpomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperpomApplication.class, args);
+	}
+
+}
